0daeda5f1b56a1783ba187cc2f377ca4676b90bb
AleksandrTurchin <107668514+AleksandrTurchin@users.noreply.github.com>
2022-12-01 21:15:07 +0300

    create models

--- department/employee/models.py
+++ department/employee/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-from hrdept import hrdept
+# import Hrdept
 
 class Employee(models.Model):
     name = models.CharField(max_length=255)
     post = models.CharField(max_length=255)
     salary = models.DecimalField(max_digits=8, decimal_places=2)
-    department = models.ForeignKey(hrdept, on_delete=models.PROTECT, null=True)
+    department = models.ForeignKey(Hrdept, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.name
 
 
+class Hrdept(models.Model):
+    department_name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.department_name
--- department/hrdept/models.py
+++ department/hrdept/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class hrdept(models.Model):
+class Hrdept(models.Model):
     department_name = models.CharField(max_length=255)
 
     def __str__(self):
