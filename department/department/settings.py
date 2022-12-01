d90b957d41a739560c416706658f733152b5996d
AleksandrTurchin <107668514+AleksandrTurchin@users.noreply.github.com>
2022-12-01 21:22:55 +0300

    create models

--- department/department/settings.py
+++ department/department/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employee',
-    'hrdept'
+    'employee.apps.EmployeeConfig',
+    'hrdept.apps.HrdeptConfig'
 ]
 
 MIDDLEWARE = [
